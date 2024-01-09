c0d9071848f82d50e8aacb3a9719e18dde3ceacd
Ameron666 <aisakebi69@gmail.com>
2024-01-09 15:06:50 +0300

    Gallery redesigned

--- js/main.js
+++ js/main.js
@@ -20,34 +20,28 @@ $(".filter-box ul").on("click", "li", function () {
 
 $(document).ready(function () {
   $(".tabs a").click(function (e) {
-      e.preventDefault();
-      $(".tabs a").removeClass("active");
-      $(this).addClass("active");
-      $(".tab-content").removeClass("active");
-      let tabId = $(this).attr("href");
-      $(tabId).addClass("active");
+    e.preventDefault();
+    $(".tabs a").removeClass("active");
+    $(this).addClass("active");
+    $(".tab-content").removeClass("active");
+    let tabId = $(this).attr("href");
+    $(tabId).addClass("active");
   });
 });
-
 
 $(document).ready(function () {
   $(".galery_imgs").on("click", ".galery_imgs__img img", function (e) {
     e.stopPropagation();
-    showModal();
+    const imageUrl = $(this).attr("src");
+    showModal(imageUrl);
   });
 
   $(".modal").click(function (e) {
-    const modalWidth = $(this).width();
-    const clickX = e.clientX - $(this).offset().left;
-
-    if (clickX < modalWidth / 2) {
-        let lists = document.querySelectorAll('.item');
-        document.getElementById('slide').prepend(lists[lists.length - 1]);
-        $("#modalImage").attr("src", $(lists[lists.length - 1]).find('img').attr("src"));
-    } else {
-        let lists = document.querySelectorAll('.item');
-        document.getElementById('slide').appendChild(lists[0]);
-        $("#modalImage").attr("src", $(lists[1]).find('img').attr("src"));
+    if (
+      !$(e.target).is("#modalImage") &&
+      !$(e.target).closest(".modal-content").length
+    ) {
+      $(this).hide();
     }
   });
 
@@ -55,8 +49,7 @@ $(document).ready(function () {
     $(".modal").hide();
   });
 
-  function showModal() {
-    const imageUrl = $(".galery_imgs__img img").attr("src");
+  function showModal(imageUrl) {
     $("#modalImage").attr("src", imageUrl);
     $(".modal").show();
   }
